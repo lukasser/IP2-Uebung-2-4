5b33411198c8e9f01cd19822158422403ffc2671
Stefan Lukasser <stefan@lukasser.eu>
2010-04-02 11:58:44 +0200

    implement find_r (recursive find), find

--- messdaten.cc
+++ messdaten.cc
@@ -1,12 +1,45 @@
 #include <iostream>
 using namespace std;
 
-struct jsw_node {
+struct node {
     int data;
-    jsw_node *link[2];
+    node *link[2];
 };
+
+struct tree {
+    node *root;
+};
+
+int find_r(node*, int);
+int find(tree*, int);
 
 int
 main() {
     return 0;
+}
+
+
+/* find_r: Recursive search
+ *
+ * Args: pointer to root, data to search for
+ * Returns: 1 if found, 0 if not found
+ *
+ */
+int
+find_r(node *root, int data) {
+    if (root == NULL) {
+        return 0; // not found
+    } else if (root->data == data) {
+        return 1; // found
+    } else {
+        int dir = root->data < data; // which subtree to follow?
+        return find_r(root->link[dir], data);
+    }
+}
+
+/* find: Convenience wrapper for find_r
+ */
+int
+find(tree *tree, int data) {
+    return find_r(tree->root, data);
 }
